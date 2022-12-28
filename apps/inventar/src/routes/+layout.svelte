6c1815e26eeedca658310de05fe16858f4f4086e
Malte2036 <malte2036@gmail.com>
2022-12-28 16:17:14 +0100

    :sparkles: design main page

--- apps/inventar/src/routes/+layout.svelte
+++ apps/inventar/src/routes/+layout.svelte
@@ -3,3 +3,13 @@
 </script>
 
 <slot />
+
+<style>
+	html,
+	body {
+		padding: 0;
+		margin: 0;
+		font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
+			Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+	}
+</style>
