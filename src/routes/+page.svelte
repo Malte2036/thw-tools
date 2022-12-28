c0ffe634de1b123659d3c11d44a05aea99079aef
Malte2036 <malte2036@gmail.com>
2022-12-28 16:48:49 +0100

    :mag: add seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,17 @@
 	import LinkButton from './LinkButton.svelte';
 </script>
 
+<svelte:head>
+	<title>THW Tools</title>
+	<meta
+		name="description"
+		content="Ein paar inoffizielle Tools für die Nutzung im THW! Unteranderem, ein AGT-Quiz und eine Anwendung zum tracken des Finnentests für Atemschutzgeräteträger."
+	/>
+</svelte:head>
+
 <div class="flex flex-col gap-4 mx-auto h-screen items-center justify-between py-4">
 	<div class="flex flex-col gap-6 items-center mt-8 px-12 w-full max-w-md">
-		<img src="THW.svg" class="w-32" alt="thw image" />
+		<img src="THW.svg" class="w-32" width="128" height="128" alt="thw image" />
 		<h1 class="w-fit text-4xl font-bold">THW Tools</h1>
 		<h2 class="w-fit text-center">Ein paar inoffizielle Tools für die Nutzung im THW!</h2>
 		<div class="w-full flex flex-col gap-4">
