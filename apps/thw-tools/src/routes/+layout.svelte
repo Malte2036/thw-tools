004bcb80a1b5aa76f3a4de517da8f82292a86d37
Malte2036 <malte2036@gmail.com>
2022-12-28 17:02:39 +0100

    :lipstick: fix height

--- apps/thw-tools/src/routes/+layout.svelte
+++ apps/thw-tools/src/routes/+layout.svelte
@@ -3,13 +3,3 @@
 </script>
 
 <slot />
-
-<style>
-	html,
-	body {
-		padding: 0;
-		margin: 0;
-		font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
-			Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-	}
-</style>
--- apps/thw-tools/src/routes/+page.svelte
+++ apps/thw-tools/src/routes/+page.svelte
@@ -10,7 +10,7 @@
 	/>
 </svelte:head>
 
-<div class="flex flex-col gap-4 mx-auto h-screen items-center justify-between py-4">
+<div class="flex flex-col gap-4 mx-auto h-full items-center justify-between py-4">
 	<div class="flex flex-col gap-6 items-center mt-8 px-12 w-full max-w-md">
 		<img src="THW.svg" class="w-32" width="128" height="128" alt="thw image" />
 		<h1 class="w-fit text-4xl font-bold">THW Tools</h1>
