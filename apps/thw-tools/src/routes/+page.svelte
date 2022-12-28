34a84ad4e4f8961b1604ab8574420c2e91e8646b
Malte2036 <malte2036@gmail.com>
2022-12-28 16:17:14 +0100

    :sparkles: design main page

--- apps/thw-tools/src/routes/+page.svelte
+++ apps/thw-tools/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import LinkButton from './LinkButton.svelte';
+</script>
+
+<div class="flex flex-col gap-4 mx-auto h-screen items-center justify-between py-4">
+	<div class="flex flex-col gap-6 items-center mt-8 px-12 w-full max-w-md">
+		<img src="THW.svg" class="w-32" alt="thw image" />
+		<h1 class="w-fit text-4xl font-bold">THW Tools</h1>
+		<h2 class="w-fit text-center">Ein paar inoffizielle Tools für die Nutzung im THW!</h2>
+		<div class="w-full flex flex-col gap-4">
+			<LinkButton name="AGT-Quiz" url="https://agt.codelam.de" />
+			<LinkButton name="Finnentest" url="https://finnentest.codelam.de" />
+		</div>
+	</div>
+	<div>©2022 Malte Sehmer</div>
+</div>
---
+++ apps/thw-tools/src/routes/LinkButton.svelte
@@ -0,0 +1,18 @@
+<script>
+	/**
+	 * @type {string}
+	 */
+	export let name;
+	/**
+	 * @type {string}
+	 */
+	export let url;
+</script>
+
+<a
+	href={url}
+	class="bg-thw text-white text-center w-full py-2 rounded-lg text-xl font-bold"
+	target="_blank"
+>
+	{name}
+</a>
