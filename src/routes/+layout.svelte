184a109576b85fc50f2a411f2175b1d6fc97ba0c
Malte2036 <malte2036@gmail.com>
2022-12-29 21:21:54 +0100

    :sparkles: add impressum

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="flex flex-col justify-between h-full">
+	<slot />
+	<div class="flex flex-row justify-center gap-2">
+		<div>©2022 Malte Sehmer</div>
+		<div class="text-gray-400">|</div>
+		<a href="/impressum" class="underline">Impressum</a>
+	</div>
+</div>
