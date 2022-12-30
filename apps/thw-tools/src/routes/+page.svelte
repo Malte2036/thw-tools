66b7994a91f588f55b48a2fdd676ee13400e6552
Malte2036 <malte2036@gmail.com>
2022-12-30 13:29:53 +0100

    :rotating_light: fix linter warnings

--- apps/thw-tools/src/routes/+page.svelte
+++ apps/thw-tools/src/routes/+page.svelte
@@ -14,7 +14,7 @@
 
 <div class="flex flex-col gap-4 mx-auto h-full items-center justify-between py-4">
 	<div class="flex flex-col gap-6 items-center mt-8 px-12 w-full max-w-md">
-		<img src="THW.svg" class="w-32" width="128" height="128" alt="thw image" />
+		<img src="THW.svg" class="w-32" width="128" height="128" alt="thw logo" />
 		<h1 class="w-fit text-4xl font-bold">THW Tools</h1>
 		<h2 class="w-fit text-center">Ein paar inoffizielle Tools für die Nutzung im THW!</h2>
 		<div class="w-full flex flex-col items-center gap-4 md:max-w-[16rem]">
