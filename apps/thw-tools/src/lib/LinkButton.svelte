da3d8765bdd8c637d6934aa2bf71a87d65a1a6cd
Malte2036 <malte2036@gmail.com>
2022-12-30 13:15:38 +0100

    :zap: prerender site

--- apps/thw-tools/src/lib/LinkButton.svelte
+++ apps/thw-tools/src/lib/LinkButton.svelte
@@ -5,8 +5,8 @@
 
 <a
 	href={url}
-	target={blank ? '_blank' : '_self'}
-	rel={blank ? 'noreffer' : ''}
+	target={blank ? '_blank' : undefined}
+	rel={blank ? 'noreffer' : undefined}
 	class="flex flex-row items-center justify-center gap-2 bg-thw text-white w-full py-2 rounded-lg text-xl font-bold"
 >
 	<slot />
