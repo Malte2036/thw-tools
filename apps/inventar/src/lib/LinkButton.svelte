cce7c73f2765c98da7d68e7e158164c21aea26c7
Malte2036 <malte2036@gmail.com>
2022-12-30 13:02:36 +0100

    :recycle: refactor

---
+++ apps/inventar/src/lib/LinkButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let url: string;
+	export let blank: boolean = false;
+</script>
+
+<a
+	href={url}
+	target={blank ? '_blank' : '_self'}
+	rel={blank ? 'noreffer' : ''}
+	class="flex flex-row items-center justify-center gap-2 bg-thw text-white w-full py-2 rounded-lg text-xl font-bold"
+>
+	<slot />
+</a>
