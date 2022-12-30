e7d5eee4b60a87af0dbca3736af24cc4cb012fd8
Malte2036 <malte2036@gmail.com>
2022-12-30 18:55:33 +0100

    :bug: fix duplicate question fetch

--- apps/thw-tools/src/routes/quiz/agt/+page.svelte
+++ apps/thw-tools/src/routes/quiz/agt/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { AGTQuestions } from './AGTQuestions';
 	import type { Question } from './Question';
 
 	let question: Question;
 	let revealAnswers = false;
 
-	assignNewQuestion();
+	onMount(() => {
+		assignNewQuestion();
+	});
 
 	function assignNewQuestion() {
 		let questionNumber = Math.floor(Math.random() * AGTQuestions.length);
@@ -27,44 +30,46 @@
 	/>
 </svelte:head>
 
-<div class="flex flex-col gap-4">
-	<h1 class="text-2xl">{question.number}. {question.text}</h1>
-	<div class="flex flex-col gap-1 ml-4">
-		{#each question.answers as answer (answer.letter)}
-			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<div
-				class="text-xl flex flex-row py-1 px-2 gap-2 rounded-md"
-				class:rightAnswer={revealAnswers && answer.correct}
-				class:wrongCheckedAnswer={revealAnswers && answer.correct != answer.checked}
-				on:click={() => (answer.checked = !answer.checked)}
-			>
-				<input type="checkbox" aria-label={answer.letter} bind:checked={answer.checked} />
-				<div>
-					{answer.letter}) {answer.text}
+{#if question}
+	<div class="flex flex-col gap-4">
+		<h1 class="text-2xl">{question.number}. {question.text}</h1>
+		<div class="flex flex-col gap-1 ml-4">
+			{#each question.answers as answer (answer.letter)}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<div
+					class="text-xl flex flex-row py-1 px-2 gap-2 rounded-md"
+					class:rightAnswer={revealAnswers && answer.correct}
+					class:wrongCheckedAnswer={revealAnswers && answer.correct != answer.checked}
+					on:click={() => (answer.checked = !answer.checked)}
+				>
+					<input type="checkbox" aria-label={answer.letter} bind:checked={answer.checked} />
+					<div>
+						{answer.letter}) {answer.text}
+					</div>
 				</div>
-			</div>
-		{/each}
+			{/each}
+		</div>
+		<button
+			on:click={() => {
+				if (revealAnswers) {
+					assignNewQuestion();
+				} else {
+					revealAnswers = !revealAnswers;
+					fetch('/api/quiz/agt/add', {
+						method: 'POST',
+						body: JSON.stringify({
+							questionId: question.number,
+							correct: question.answers.every((answer) => answer.checked == answer.correct)
+						}),
+						headers: { 'content-type': 'application/json' }
+					});
+				}
+			}}
+			class="bg-thw text-white py-2 rounded-lg text-xl font-bold"
+			>{revealAnswers ? 'Nächste Frage' : 'Überprüfen'}</button
+		>
 	</div>
-	<button
-		on:click={() => {
-			if (revealAnswers) {
-				assignNewQuestion();
-			} else {
-				revealAnswers = !revealAnswers;
-				fetch('/api/quiz/agt/add', {
-					method: 'POST',
-					body: JSON.stringify({
-						questionId: question.number,
-						correct: question.answers.every((answer) => answer.checked == answer.correct)
-					}),
-					headers: { 'content-type': 'application/json' }
-				});
-			}
-		}}
-		class="bg-thw text-white py-2 rounded-lg text-xl font-bold"
-		>{revealAnswers ? 'Nächste Frage' : 'Überprüfen'}</button
-	>
-</div>
+{/if}
 
 <style>
 	.rightAnswer {
