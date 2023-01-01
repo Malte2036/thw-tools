f7b56cc36d9a4f749d0b9f461689108b766a36cc
Malte2036 <malte2036@gmail.com>
2023-01-02 00:23:19 +0100

    :lipstick: redesign quiz page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,19 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				thw: '#120a8f'
+				thw: {
+					DEFAULT: '#120a8f',
+					50: '#f1f3ff',
+					100: '#e6e9ff',
+					200: '#d0d7ff',
+					300: '#abb4ff',
+					400: '#7b83ff',
+					500: '#4647ff',
+					600: '#2b20ff',
+					700: '#1b0ef3',
+					800: '#160bcc',
+					900: '#120a8f'
+				}
 			}
 		}
 	},
