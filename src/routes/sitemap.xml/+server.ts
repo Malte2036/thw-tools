db58e02f1d06893dbf09ff56ba3abd721652ee75
Malte2036 <malte2036@gmail.com>
2023-01-01 14:04:36 +0100

    :mag: add sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,28 @@
+export async function GET() {
+	return new Response(
+		`
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <urlset
+        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="https://www.w3.org/1999/xhtml"
+        xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+      >
+        <url>
+            <loc>https://thw-tools.de/</loc>
+            <priority>1.00</priority>
+        </url>
+        <url>
+            <loc>https://thw-tools.de/quiz/agt</loc>
+            <priority>0.80</priority>
+        </url>
+      </urlset>`.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		}
+	);
+}
