1480f37f6be31392f47e8a588661ed8921037cb5
Malte2036 <malte2036@gmail.com>
2023-01-01 13:46:47 +0100

    :lipstick: add navbar

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import LinkButton from '$lib/LinkButton.svelte';
+	import { page } from '$app/stores';
+
+	let title = 'THW-Tools';
+
+	switch ($page.url.pathname) {
+		case '/quiz/agt':
+			title = 'Atemschutz-Quiz';
+			break;
+	}
+</script>
+
+<div class="flex flex-col gap-4">
+	<div
+		class="flex flex-row justify-between items-center w-full h-12 border-b-[1px] border-black mt-4 px-4 pb-4"
+	>
+		<div class="flex flex-row gap-2 items-center">
+			<a href="/">
+				<img src="../THW.svg" class="h-12 w-fit" width="128" height="128" alt="thw logo" /></a
+			>
+			<h1 class="text-4xl max-md:text-3xl max-sm:text-2xl font-bold">{title}</h1>
+		</div>
+		<div class="max-sm:hidden">
+			<LinkButton url="/">Mehr THW Tools</LinkButton>
+		</div>
+	</div>
+
+	<slot />
+</div>
