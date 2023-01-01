0c01fa36b537f54e57f528806a88aacd458cded3
Malte2036 <malte2036@gmail.com>
2023-01-01 13:55:29 +0100

    :lipstick: make navbar icon and title to link

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -15,12 +15,12 @@
 	<div
 		class="flex flex-row justify-between items-center w-full h-12 border-b-[1px] border-black mt-4 px-4 pb-4"
 	>
-		<div class="flex flex-row gap-2 items-center">
-			<a href="/">
-				<img src="../THW.svg" class="h-12 w-fit" width="128" height="128" alt="thw logo" /></a
-			>
+		<a href="/" class="flex flex-row gap-2 items-center">
+			<div>
+				<img src="../THW.svg" class="h-12 w-fit" width="128" height="128" alt="thw logo" />
+			</div>
 			<h1 class="text-4xl max-md:text-3xl max-sm:text-2xl font-bold">{title}</h1>
-		</div>
+		</a>
 		<div class="max-sm:hidden">
 			<LinkButton url="/">Mehr THW Tools</LinkButton>
 		</div>
