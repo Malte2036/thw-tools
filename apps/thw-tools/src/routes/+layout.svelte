e75a47471b2c42d2f36b68eff43c64b48b5d477b
Malte2036 <malte2036@gmail.com>
2023-01-05 17:24:01 +0100

    :sparkles: add global error page

--- apps/thw-tools/src/routes/+layout.svelte
+++ apps/thw-tools/src/routes/+layout.svelte
@@ -1,9 +1,28 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import { page } from '$app/stores';
+	import Header from './Header.svelte';
+
+	let title: string | undefined;
+	$: title = getCurrentTitleByPath($page.url.pathname);
+
+	function getCurrentTitleByPath(path: string): string | undefined {
+		switch (path) {
+			case '/quiz/agt':
+				return 'Atemschutz-Quiz';
+			case '/':
+			case '/impressum':
+				return undefined;
+			default:
+				return 'THW-Tools';
+		}
+	}
 </script>
 
 <div class="flex flex-col gap-4 justify-between min-h-screen">
+	{#if title !== undefined}
+		<Header {title} />
+	{/if}
 	<div class="grow">
 		<slot />
 	</div>
