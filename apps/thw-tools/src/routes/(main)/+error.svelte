8d148209c82986b9be8b8a2448eb3962ac652081
Malte2036 <malte2036@gmail.com>
2023-01-05 08:45:35 +0100

    :sparkles: add quiz error page

---
+++ apps/thw-tools/src/routes/(main)/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="h-full flex flex-col items-center gap-4 m-4">
+	<h1 class="text-3xl font-bold">{$page.status} Not Found</h1>
+	<h2 class="text-2xl">{$page.error.message}</h2>
+</div>
