c1b806f43849de5b9cd01f924d2ec77d16e2e87d
Malte2036 <malte2036@gmail.com>
2023-01-05 18:03:29 +0100

    :lipstick: small hover changes

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -11,7 +11,7 @@
 		<div>
 			<img src="../THW.svg" class="h-12 w-fit" width="128" height="128" alt="thw logo" />
 		</div>
-		<h1 class="text-4xl max-md:text-3xl max-sm:text-2xl font-bold">{title}</h1>
+		<h1 class="text-4xl max-md:text-3xl max-sm:text-2xl font-bold hover:text-thw">{title}</h1>
 	</a>
 	<div class="max-sm:hidden">
 		<LinkButton url="/">Mehr THW Tools</LinkButton>
