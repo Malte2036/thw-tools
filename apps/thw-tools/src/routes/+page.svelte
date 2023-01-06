bb7b834d04d19cf218bad1d7280387d2ad606a4a
Malte2036 <malte2036@gmail.com>
2023-01-06 18:03:03 +0100

    :lipstick: improve index page font size

--- apps/thw-tools/src/routes/+page.svelte
+++ apps/thw-tools/src/routes/+page.svelte
@@ -2,22 +2,24 @@
 	import ChartSimpleIcon from '../lib/icons/ChartSimpleIcon.svelte';
 	import HearthPulseIcon from '../lib/icons/HearthPulseIcon.svelte';
 	import LinkButton from '../lib/LinkButton.svelte';
+
+	const description =
+		'Ein paar inoffizielle Tools für die Nutzung im THW! Unteranderem, ein AGT-Quiz und eine Anwendung zum tracken des Finnentests für Atemschutzgeräteträger.';
 </script>
 
 <svelte:head>
 	<title>THW Tools</title>
-	<meta
-		name="description"
-		content="Ein paar inoffizielle Tools für die Nutzung im THW! Unteranderem, ein AGT-Quiz und eine Anwendung zum tracken des Finnentests für Atemschutzgeräteträger."
-	/>
+	<meta name="description" content={description} />
 </svelte:head>
 
-<div class="flex flex-col gap-4 mx-auto h-full items-center justify-between py-4">
-	<div class="flex flex-col gap-6 items-center mt-8 px-12 w-full max-w-md">
-		<img src="thw-mzgw.png" class="w-64" width="248" alt="THW MehrzweckGerätewagen" />
-		<h1 class="w-fit text-4xl font-bold">THW Tools</h1>
-		<h2 class="w-fit text-center">Ein paar inoffizielle Tools für die Nutzung im THW!</h2>
-		<div class="w-full flex flex-col items-center gap-4 md:max-w-[16rem]">
+<div class="flex flex-col gap-4 mx-auto h-full items-center justify-between mt-16 max-md:mt-8 py-4">
+	<div class="flex flex-col gap-8 items-center px-12 w-full max-w-4xl">
+		<img src="thw-mzgw.png" class="w-96 max-md:w-64" width="384" alt="THW MehrzweckGerätewagen" />
+		<h1 class="w-fit text-5xl max-md:text-4xl font-bold">THW Tools</h1>
+		<h2 class="w-fit text-2xl max-md:text-lg text-center">
+			{description}
+		</h2>
+		<div class="w-full flex flex-col items-center gap-4 max-w-sm max-md:max-w-[16rem]">
 			<LinkButton url="/quiz/agt">
 				<div class="w-6">
 					<ChartSimpleIcon />
