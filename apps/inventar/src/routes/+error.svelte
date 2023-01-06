83db78f7f4508628c0c0811175d14e36223950b0
Malte2036 <malte2036@gmail.com>
2023-01-06 18:16:37 +0100

    :rotating_light: fix compiler / linter warnings

--- apps/inventar/src/routes/+error.svelte
+++ apps/inventar/src/routes/+error.svelte
@@ -4,5 +4,5 @@
 
 <div class="h-full flex flex-col items-center gap-4 m-4">
 	<h1 class="text-3xl font-bold">{$page.status} Not Found</h1>
-	<h2 class="text-2xl">{$page.error.message}</h2>
+	<h2 class="text-2xl">{$page.error != null ? $page.error.message : 'Unknown Error'}</h2>
 </div>
