eaafcaa416bc981d61761e98ba24497b242c34e4
Malte2036 <malte2036@gmail.com>
2023-01-06 18:27:22 +0100

    :zap: add logo image processing

--- apps/thw-tools/src/routes/+page.svelte
+++ apps/thw-tools/src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import ChartSimpleIcon from '../lib/icons/ChartSimpleIcon.svelte';
 	import HearthPulseIcon from '../lib/icons/HearthPulseIcon.svelte';
 	import LinkButton from '../lib/LinkButton.svelte';
+	import logo from '$lib/icons/thw-mzgw.webp';
 
 	const description =
 		'Ein paar inoffizielle Tools für die Nutzung im THW! Unteranderem, ein AGT-Quiz und eine Anwendung zum tracken des Finnentests für Atemschutzgeräteträger.';
@@ -14,7 +15,13 @@
 
 <div class="flex flex-col gap-4 mx-auto h-full items-center justify-between mt-16 max-md:mt-8 py-4">
 	<div class="flex flex-col gap-8 items-center px-12 w-full max-w-4xl">
-		<img src="thw-mzgw.png" class="w-96 max-md:w-64" width="384" alt="THW MehrzweckGerätewagen" />
+		<img
+			src={logo}
+			class="w-96 max-md:w-64"
+			width="384"
+			height="308.42"
+			alt="THW MehrzweckGerätewagen"
+		/>
 		<h1 class="w-fit text-5xl max-md:text-4xl font-bold">THW Tools</h1>
 		<h2 class="w-fit text-2xl max-md:text-lg text-center">
 			{description}
