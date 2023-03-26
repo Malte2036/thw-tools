292e6560a0f32aa8c55a613e8da0e7a868990cf2
Malte2036 <malte2036@gmail.com>
2023-03-26 20:34:58 +0200

    :recycle: refactor QuestionNumber

---
+++ src/lib/quiz/QuestionNumber.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let questionNumber: number;
+	export let questionCount: number;
+</script>
+
+<h3 class="flex flex-row justify-center text-gray-400 font-bold">
+	{questionNumber}/{questionCount}
+</h3>
