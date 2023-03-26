a6c2b977459a5ab6c56492cae55ca6c43b9dd264
Malte2036 <malte2036@gmail.com>
2023-03-26 17:25:18 +0200

    :construction: begin cbrn

---
+++ src/routes/(main)/quiz/[type]/CBRNQuestions.ts
@@ -0,0 +1,88 @@
+import type { Question } from './Question';
+
+export const CBRNQuestions: Question[] = [
+	{
+		answers: [
+			{
+				text: 'Gefahrgut sind Stoffe und Gegenstände, von denen auf Grund ihrer Natur, ihrer Eigenschaften oder ihres Zustandes im Zusammenhang mit der Beförderung Gefahren ausgehen können.',
+				correct: true
+			},
+			{ text: 'Geld und wertvolle Antiquitäten.', correct: false },
+			{ text: 'Getränke für die Einheiten.', correct: false }
+		],
+		number: 1,
+		text: 'Was ist ein Gefahrgut?'
+	},
+	{
+		answers: [
+			{
+				text: 'Gefahrstoffe sind Stoffe, Zubereitungen und Erzeugnisse, die bestimmte gefährliche Eigenschaften aufweisen oder von denen chemisch-physikalische Gefahren ausgehen.',
+				correct: true
+			},
+			{
+				text: 'Gefahrstoffe sind Stoffe die nur selten Helfer gefährlich werden können.',
+				correct: false
+			},
+			{
+				text: 'Gefahrstoffe sind Stoffe, für die ein Helfer keine Schutzbekleidung benötigt.',
+				correct: false
+			}
+		],
+		number: 2,
+		text: 'Was sind Gefahrstoffe?'
+	},
+	{
+		answers: [
+			{
+				text: 'Jede Helferin / jeder Helfer im THW.',
+				correct: true
+			},
+			{
+				text: 'Nur die Präsidentin / der Präsident des THW.',
+				correct: false
+			},
+			{
+				text: 'Nur der Auftraggeber.',
+				correct: false
+			}
+		],
+		number: 3,
+		text: 'Wer kann für Vergehen im Umgang mit gefährlichen Stoffen und Gütern zur Verantwortung gezogen werden?'
+	},
+	{
+		answers: [
+			{
+				text: 'Möglichst großen Abstand zum Unfallort halten, alle Zündquellen fernhalten (Motor abstellen, Rauchverbot, etc.) und die Strasse sichern (Warnschilder aufstellen), Straßenbenutzer sowie Anwohner warnen.',
+				correct: true
+			},
+			{
+				text: 'Verunfallte wenn möglich aus dem gefährlichen Bereich bringen (Schutzmaßnahmen unbedingt beachten – Eigenschutz!).',
+				correct: true
+			},
+			{
+				text: 'Es müssen keine Schutzmaßnahmen getroffen werden.',
+				correct: false
+			}
+		],
+		number: 4,
+		text: 'Was ist bei einem Unfall mit gefährlichen Stoffen und Gütern zu beachten?'
+	},
+	{
+		answers: [
+			{
+				text: 'Kennzeichnung an den Verpackungen.',
+				correct: true
+			},
+			{
+				text: 'Fahrzeugkennzeichnung, mitgeführte Fahrzeugpapiere und Merkblätter.',
+				correct: true
+			},
+			{
+				text: 'Durch Fahrerbefragung.',
+				correct: false
+			}
+		],
+		number: 5,
+		text: 'Woran kann ich die Gefährdung, die von einem Gefahrguttransport ausgeht, eindeutig erkennen?'
+	}
+];
