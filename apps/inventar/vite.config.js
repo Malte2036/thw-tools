d4fb142d0e92797962466c6dd9c4ccb4085e3d6e
Malte2036 <malte2036@gmail.com>
2023-03-27 11:14:11 +0200

    :bug: fix pwa offline

--- apps/inventar/vite.config.js
+++ apps/inventar/vite.config.js
@@ -6,6 +6,13 @@ const config = {
 	plugins: [
 		sveltekit(),
 		SvelteKitPWA({
+			// add this to cache all the imports
+			workbox: {
+				globPatterns: ['src/**/*']
+			},
+			// add this to cache all the
+			// static assets in the public folder
+			includeAssets: ['src/lib/assets/**/*', 'src/lib/icons/**/*'],
 			manifest: {
 				theme_color: '#120a8f',
 				background_color: 'white',
