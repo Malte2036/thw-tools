c86f48f747c56770775d1b8cd9cb5b2a6cd7711b
Malte2036 <malte2036@gmail.com>
2023-03-27 15:10:45 +0200

    :zap: redirect to question 1

--- src/routes/(main)/quiz/[type]/+page.server.ts
+++ src/routes/(main)/quiz/[type]/+page.server.ts
@@ -6,12 +6,11 @@ export const prerender = true;
 
 export const load = (async ({ params }) => {
 	const questionType: QuestionType | undefined = params.type as QuestionType;
-	const questionSet = questionTypeToQuestionSet(questionType);
+	//const questionSet = questionTypeToQuestionSet(questionType);
 
-	const questionNumber: number = Math.floor(Math.random() * questionSet.length) + 1;
+	const questionNumber: number = 1; /*Math.floor(Math.random() * questionSet.length) + 1;*/
 
-	return {
-		questionType,
-		questionNumber
-	};
+	const url = `/quiz/${questionType}/${questionNumber}`;
+
+	throw redirect(300, url);
 }) satisfies PageLoad;
