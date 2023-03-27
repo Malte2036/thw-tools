08e0b09b74f295eaa983235ae52419a88337d9b8
Malte2036 <malte2036@gmail.com>
2023-03-27 15:59:49 +0200

    :bug: fix redirect

--- apps/thw-tools/src/routes/(main)/quiz/[type]/+page.svelte
+++ apps/thw-tools/src/routes/(main)/quiz/[type]/+page.svelte
@@ -1,15 +1,1 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-
-	onMount(() => {
-		const url = `/quiz/${data.questionType}/${data.questionNumber}`;
-		console.log(`redirect to ${url}`);
-
-		goto(url);
-	});
-</script>
+Redirecting...
