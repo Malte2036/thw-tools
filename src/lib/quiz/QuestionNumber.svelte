738fe51eb907852c3dbf9610857e804474bd1cc5
Malte2036 <malte2036@gmail.com>
2023-03-27 11:33:54 +0200

    :sparkles: select quiz number input

--- src/lib/quiz/QuestionNumber.svelte
+++ src/lib/quiz/QuestionNumber.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	export let questionNumber: number;
 	export let questionCount: number;
+	export let gotoQuestionNumber: (newQuestionNumber: number) => void;
+
+	function askForQuestionNumber() {
+		const newQuestionNumber = Number.parseInt(
+			prompt('Enter a question number:', String(questionNumber)) ?? ''
+		);
+
+		gotoQuestionNumber(newQuestionNumber);
+	}
 </script>
 
-<h3 class="flex flex-row justify-center text-gray-400 font-bold">
+<h3 class="flex flex-row justify-center text-gray-400 font-bold" on:click={askForQuestionNumber}>
 	{questionNumber}/{questionCount}
 </h3>
