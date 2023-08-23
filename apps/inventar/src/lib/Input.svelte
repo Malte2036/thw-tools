d2fa61f7950c6a209c19505fcbf72ee2d0bd269a
Malte2036 <malte2036@gmail.com>
2023-08-24 00:07:51 +0200

    :sparkles: First prototype of protective cbrn suite calculation

---
+++ apps/inventar/src/lib/Input.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let inputValue: string;
+
+	export let label: string | undefined = undefined;
+	export let placeholder: string | undefined = undefined;
+</script>
+
+<div class="flex flex-col gap-1">
+	{#if label !== undefined}
+		<label class="font-bold" for="inputField">{label}</label>
+	{/if}
+
+	<input
+		class="border border-black rounded-lg py-1 px-2"
+		id="inputField"
+		bind:value={inputValue}
+		{placeholder}
+	/>
+</div>
