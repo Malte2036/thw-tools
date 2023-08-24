72d5a901a23e1d0cab8271f9b94886f3bd5a6edf
Malte2036 <malte2036@gmail.com>
2023-08-24 11:48:05 +0200

    :sparkles: Add "More THW Tools" Button to mobile version

--- apps/inventar/src/routes/Header.svelte
+++ apps/inventar/src/routes/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import LinkButton from '$lib/LinkButton.svelte';
+	import HomeIcon from '$lib/icons/HomeIcon.svelte';
 	import logo from '$lib/icons/thw-mzgw.webp';
 
 	export let title: string;
@@ -20,7 +21,12 @@
 		</div>
 		<h1 class="text-4xl max-md:text-3xl max-sm:text-2xl font-bold hover:text-thw">{title}</h1>
 	</a>
-	<div class="max-sm:hidden">
-		<LinkButton url="/">Mehr THW Tools</LinkButton>
+	<div>
+		<LinkButton url="/" dataUmamiEvent={'Back to Main Page'}>
+			<div class="flex flex-row gap-2 items-center justify-center">
+				<HomeIcon />
+				<div class="max-sm:hidden">Mehr THW Tools</div>
+			</div>
+		</LinkButton>
 	</div>
 </div>
