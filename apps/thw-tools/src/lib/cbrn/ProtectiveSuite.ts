dff8781ae078ec7a51acf8785a9bd47e7654d430
Malte2036 <malte2036@gmail.com>
2023-08-24 11:35:10 +0200

    :sparkles: Add gasTight to protectiveSuite

--- apps/thw-tools/src/lib/cbrn/ProtectiveSuite.ts
+++ apps/thw-tools/src/lib/cbrn/ProtectiveSuite.ts
@@ -2,6 +2,7 @@ import { allSubstances, type Substance } from './Substance';
 
 export type ProtectiveSuite = {
 	name: string;
+	gasTight: boolean | undefined;
 	substancesProtection: Map<Substance, string>;
 };
 
@@ -10,6 +11,7 @@ export type ProtectiveSuite = {
 export const allProtectiveSuites: ProtectiveSuite[] = [
 	{
 		name: 'ESK 1 PE',
+		gasTight: false,
 		substancesProtection: new Map<Substance, string>([
 			[allSubstances.find((e) => e.name == 'Ammoniak')!, '1a'],
 			[allSubstances.find((e) => e.name == 'Ammoniumhydrogencarbonat')!, '1a']
@@ -17,10 +19,25 @@ export const allProtectiveSuites: ProtectiveSuite[] = [
 	},
 	{
 		name: 'ESK S3 PE',
+		gasTight: undefined,
 		substancesProtection: new Map<Substance, string>([
 			[allSubstances.find((e) => e.name == 'Ammoniak')!, '2a'],
 			[allSubstances.find((e) => e.name == 'Ammoniumhydrogencarbonat')!, '3c'],
 			[allSubstances.find((e) => e.name == 'Ammoniumnitratdünger')!, '1b']
 		])
+	},
+	{
+		name: 'Dummy',
+		gasTight: true,
+		substancesProtection: new Map<Substance, string>([])
 	}
 ];
+
+export function getDescriptionByProtectiveSuite(protectiveSuite: ProtectiveSuite) {
+	let gasTightDescription = '';
+	if (protectiveSuite.gasTight !== undefined) {
+		gasTightDescription = ` und ist ${protectiveSuite.gasTight ? 'gasdicht' : 'nicht gasdicht'}`;
+	}
+
+	return `Der Schutzanzug ${protectiveSuite.name} ist ein Schutzanzug der Klasse ?${gasTightDescription}.`;
+}
