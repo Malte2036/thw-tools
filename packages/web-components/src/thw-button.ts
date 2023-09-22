21633b4dc90a8b0002e97805d0594492b8c336c2
Malte2036 <malte2036@gmail.com>
2023-09-22 14:01:48 +0200

    :sparkles: Add size to thw-button

--- packages/web-components/src/thw-button.ts
+++ packages/web-components/src/thw-button.ts
@@ -7,6 +7,8 @@ export class THWButton extends LitElement {
   @property({ type: String }) type = "primary";
   @property({ type: Boolean }) disabled = false;
 
+  @property({ type: String }) size = "normal";
+
   static styles = [
     css`
       button {
@@ -15,10 +17,23 @@ export class THWButton extends LitElement {
 
         border-color: ${unsafeCSS(thwColors[1000])};
 
-        border-radius: 0.25rem;
+        border-radius: 0.5rem;
         border-width: 1px;
         padding: 0.5rem 1rem;
         cursor: pointer;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5rem;
+
+        width: 100%;
+
+        &.big {
+          font-size: 1.25rem;
+          line-height: 1.75rem;
+        }
 
         &:hover {
           background-color: ${unsafeCSS(thwColors[800])};
@@ -26,6 +41,10 @@ export class THWButton extends LitElement {
 
         &:disabled {
           cursor: not-allowed;
+          background-color: white;
+          color: ${unsafeCSS(grayColors[400])};
+
+          border-color: black;
         }
 
         &.secondary {
@@ -49,7 +68,7 @@ export class THWButton extends LitElement {
 
   render() {
     return html`
-      <button ?disabled=${this.disabled} class=${this.type}>
+      <button ?disabled=${this.disabled} class=${`${this.type} ${this.size}`}>
         <slot></slot>
       </button>
     `;
