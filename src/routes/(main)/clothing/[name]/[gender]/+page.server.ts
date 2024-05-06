9652b25d36b54c03d68ba2b73e64ade7a8e85164
Malte2036 <malte2036@gmail.com>
2024-05-06 18:01:18 +0200

    :bug: Fix cannot access query param in ssr

--- src/routes/(main)/clothing/[name]/[gender]/+page.server.ts
+++ src/routes/(main)/clothing/[name]/[gender]/+page.server.ts
@@ -1,3 +1,4 @@
+import { browser } from '$app/environment';
 import type { HumanGender } from '$lib/clothing/clothing';
 import { loadClothingSizesTables } from '$lib/clothing/clothingUtils';
 import type { PageServerLoad } from '../$types';
@@ -7,9 +8,10 @@ export const prerender = true;
 export const load = (async ({ params, url }) => {
 	const name = params.name as string;
 	const gender = params.gender as HumanGender;
-	const size: number | undefined = url.searchParams.has('size')
-		? parseInt(url.searchParams.get('size') as string)
-		: undefined;
+	const size: number | undefined =
+		browser && url.searchParams.has('size')
+			? parseInt(url.searchParams.get('size') as string)
+			: undefined;
 
 	const tables = await loadClothingSizesTables();
 
