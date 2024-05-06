5516dc201dc1521b0c91fb31cf7538ce9f12d8e8
Malte2036 <malte2036@gmail.com>
2024-05-06 18:48:27 +0200

    :sparkles: Show clothing input values on clothing page

--- src/lib/clothing/ClothingHead.svelte
+++ src/lib/clothing/ClothingHead.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { ClothingSizesTable } from './clothing';
-	import { clothingNameToClothingType, clothingNameToFriendlyName } from './clothingUtils';
+	import { clothingNameToFriendlyName } from './clothingUtils';
 
 	export let table: ClothingSizesTable | undefined = undefined;
 	let title = `THW-Tools: ${
