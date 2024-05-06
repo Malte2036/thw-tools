aaa63e34a808220b093bd179d58ec924a59101ad
Malte2036 <malte2036@gmail.com>
2024-05-06 19:04:01 +0200

    :sparkles: Persist clothing input in localStorage

--- src/lib/clothing/clothingInputStore.ts
+++ src/lib/clothing/clothingInputStore.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 import type { HumanGender } from './clothing';
+import { browser } from '$app/environment';
 
 export type ClothingInputValue = {
 	gender: HumanGender;
@@ -10,11 +11,34 @@ export type ClothingInputValue = {
 	insideLegLength: string;
 };
 
-export const clothingInput = writable<ClothingInputValue>({
+const defaultClothingInput: ClothingInputValue = {
 	gender: 'M',
 	height: '',
 	chest: '',
 	waist: '',
 	hip: '',
 	insideLegLength: ''
+};
+
+const localStorageKey = 'clothingInput';
+function getSavedClothingInput(): ClothingInputValue {
+	if (!browser) return defaultClothingInput;
+
+	const storedValue = localStorage.getItem(localStorageKey);
+	if (!storedValue) return defaultClothingInput;
+
+	try {
+		return JSON.parse(storedValue);
+	} catch (e) {
+		console.error('Error parsing clothing input from local storage', e);
+		return defaultClothingInput;
+	}
+}
+
+export const clothingInput: any = writable<ClothingInputValue>(getSavedClothingInput(), () => {
+	if (!browser) return;
+
+	return clothingInput.subscribe((value: ClothingInputValue) =>
+		localStorage.setItem(localStorageKey, JSON.stringify(value))
+	);
 });
