440c39e08a7812c9bfa211bd3a849e7695c831c3
Malte2036 <malte2036@gmail.com>
2024-05-30 10:50:56 +0200

    :recycle: Refactor clothing page

---
+++ apps/thw-tools/src/lib/clothing/ClothingResultCard.svelte
@@ -0,0 +1,159 @@
+<script lang="ts">
+	import LinkButton from '$lib/LinkButton.svelte';
+	import type {
+		ClothingMeasurementImportance,
+		ClothingName,
+		ClothingSizes,
+		HumanMeasurement,
+		MatchingClothingSizeTable
+	} from './clothing';
+	import {
+		clothingNameToFriendlyName,
+		humanMeasurementToFriendlyName
+	} from './clothingConstantUtils';
+	import { isDeviationAcceptable } from './clothingUtils';
+
+	export let size: MatchingClothingSizeTable;
+
+	export let missingMeasurements: Map<ClothingName, HumanMeasurement[]> = new Map();
+
+	function sizeToString(
+		humanMeasurement: HumanMeasurement,
+		matchingClothingSize: ClothingSizes | undefined,
+		measurementImportance: ClothingMeasurementImportance[]
+	) {
+		const friendlyName = humanMeasurementToFriendlyName(humanMeasurement);
+		if (!matchingClothingSize) return `${friendlyName}: -`;
+
+		const sizeValue = matchingClothingSize[humanMeasurement];
+		if (!sizeValue) return `${friendlyName}: -`;
+
+		const isToleranceAllowed = isToleranceAllowedInMeasurement(
+			humanMeasurement,
+			measurementImportance
+		);
+
+		if (isToleranceAllowed === false) {
+			return `${friendlyName}: ${sizeValue.min} - ${sizeValue.max} cm (genau)`;
+		}
+
+		return `${friendlyName}: ${sizeValue.min} - ${sizeValue.max} cm`;
+	}
+
+	function isNessaryMeasurement(
+		humanMeasurement: HumanMeasurement,
+		measurementImportance: ClothingMeasurementImportance[]
+	): boolean | undefined {
+		const importance = measurementImportance.find(
+			(importance) => importance.measurement === humanMeasurement
+		);
+
+		return importance !== undefined;
+	}
+
+	function isToleranceAllowedInMeasurement(
+		humanMeasurement: HumanMeasurement,
+		measurementImportance: ClothingMeasurementImportance[]
+	): boolean | undefined {
+		const importance = measurementImportance.find(
+			(importance) => importance.measurement === humanMeasurement
+		);
+		if (!importance) return undefined;
+
+		return importance.allowTolerance;
+	}
+
+	function getTableLink(size: MatchingClothingSizeTable): string {
+		let link = `${size.name}/${size.gender}`;
+		const matchingClothingSize = size.matchingClothingSizes[0];
+		if (matchingClothingSize && isDeviationAcceptable(matchingClothingSize.deviation)) {
+			link += `?size=${matchingClothingSize.clothingSize.size}`;
+		}
+
+		return link;
+	}
+</script>
+
+<div class="flex flex-col justify-between gap-1 border-2 p-2 border-thw rounded-md">
+	<div>
+		<h2 class="text-xl font-bold">{clothingNameToFriendlyName(size.name)}:</h2>
+		{#if size.matchingClothingSizes && isDeviationAcceptable(size.matchingClothingSizes[0].deviation)}
+			<div class="flex flex-col gap-2">
+				<div>
+					<p>Konfektionsgröße: {size.matchingClothingSizes[0].clothingSize.size}</p>
+					<p class={isNessaryMeasurement('height', size.measurementImportance) ? 'font-bold' : ''}>
+						{sizeToString(
+							'height',
+							size.matchingClothingSizes[0].clothingSize,
+							size.measurementImportance
+						)}
+					</p>
+					<p
+						class={isNessaryMeasurement('chestCircumference', size.measurementImportance)
+							? 'font-bold'
+							: ''}
+					>
+						{sizeToString(
+							'chestCircumference',
+							size.matchingClothingSizes[0].clothingSize,
+							size.measurementImportance
+						)}
+					</p>
+					<p
+						class={isNessaryMeasurement('waistCircumference', size.measurementImportance)
+							? 'font-bold'
+							: ''}
+					>
+						{sizeToString(
+							'waistCircumference',
+							size.matchingClothingSizes[0].clothingSize,
+							size.measurementImportance
+						)}
+					</p>
+					<p
+						class={isNessaryMeasurement('hipCircumference', size.measurementImportance)
+							? 'font-bold'
+							: ''}
+					>
+						{sizeToString(
+							'hipCircumference',
+							size.matchingClothingSizes[0].clothingSize,
+							size.measurementImportance
+						)}
+					</p>
+					<p
+						class={isNessaryMeasurement('insideLegLength', size.measurementImportance)
+							? 'font-bold'
+							: ''}
+					>
+						{sizeToString(
+							'insideLegLength',
+							size.matchingClothingSizes[0].clothingSize,
+							size.measurementImportance
+						)}
+					</p>
+				</div>
+				<div class="italic text-sm">
+					[Abweichung: {size.matchingClothingSizes[0].deviation.toFixed(2)}]
+				</div>
+			</div>
+		{:else if missingMeasurements.has(size.name)}
+			<p class="font-bold text-red-500">Fehlende Maße:</p>
+			<ul>
+				{#each missingMeasurements.get(size.name) ?? [] as missingMeasurement}
+					<li>
+						{humanMeasurementToFriendlyName(missingMeasurement)}
+						{#if isToleranceAllowedInMeasurement(missingMeasurement, size.measurementImportance) === false}
+							(genau)
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<p>No matching sizes found.</p>
+		{/if}
+	</div>
+	<div>
+		<LinkButton url={getTableLink(size)} secondary>Maßtabelle ansehen</LinkButton>
+	</div>
+</div>
--- apps/thw-tools/src/routes/(main)/clothing/+page.svelte
+++ apps/thw-tools/src/routes/(main)/clothing/+page.svelte
@@ -1,29 +1,21 @@
 <script lang="ts">
 	import Button from '$lib/Button.svelte';
-	import Input from '$lib/Input.svelte';
-	import LinkButton from '$lib/LinkButton.svelte';
-	import Select from '$lib/Select.svelte';
 	import ClothingExportDialog from '$lib/clothing/ClothingExportDialog.svelte';
 	import ClothingHead from '$lib/clothing/ClothingHead.svelte';
+	import ClothingResultCard from '$lib/clothing/ClothingResultCard.svelte';
+	import ClothingSizesInput from '$lib/clothing/ClothingSizesInput.svelte';
 	import type {
-		ClothingMeasurementImportance,
 		ClothingName,
-		ClothingSizes,
 		HumanMeasurement,
 		MatchingClothingSizeTable
 	} from '$lib/clothing/clothing';
-	import {
-		clothingNameToFriendlyName,
-		humanGenderToFriendlyString,
-		humanMeasurementToFriendlyName
-	} from '$lib/clothing/clothingConstantUtils';
+	import { clothingNameToFriendlyName } from '$lib/clothing/clothingConstantUtils';
 	import { convertClothingResultsToCSV, exportCSVFile } from '$lib/clothing/clothingExport';
 	import type { ClothingInputValue } from '$lib/clothing/clothingInputStore';
 	import { clothingInput } from '$lib/clothing/clothingInputStore';
 	import {
 		calculateMatchingClothingSizeForTables,
-		getMissingMeasurements,
-		isDeviationAcceptable
+		getMissingMeasurements
 	} from '$lib/clothing/clothingUtils';
 	import { bannerMessage } from '$lib/shared/stores/bannerMessage';
 	import type { PageData } from './$types';
@@ -54,62 +46,6 @@
 	}
 
 	$: calculate($clothingInput);
-
-	function sizeToString(
-		humanMeasurement: HumanMeasurement,
-		matchingClothingSize: ClothingSizes | undefined,
-		measurementImportance: ClothingMeasurementImportance[]
-	) {
-		const friendlyName = humanMeasurementToFriendlyName(humanMeasurement);
-		if (!matchingClothingSize) return `${friendlyName}: -`;
-
-		const sizeValue = matchingClothingSize[humanMeasurement];
-		if (!sizeValue) return `${friendlyName}: -`;
-
-		const isToleranceAllowed = isToleranceAllowedInMeasurement(
-			humanMeasurement,
-			measurementImportance
-		);
-
-		if (isToleranceAllowed === false) {
-			return `${friendlyName}: ${sizeValue.min} - ${sizeValue.max} cm (genau)`;
-		}
-
-		return `${friendlyName}: ${sizeValue.min} - ${sizeValue.max} cm`;
-	}
-
-	function isNessaryMeasurement(
-		humanMeasurement: HumanMeasurement,
-		measurementImportance: ClothingMeasurementImportance[]
-	): boolean | undefined {
-		const importance = measurementImportance.find(
-			(importance) => importance.measurement === humanMeasurement
-		);
-
-		return importance !== undefined;
-	}
-
-	function isToleranceAllowedInMeasurement(
-		humanMeasurement: HumanMeasurement,
-		measurementImportance: ClothingMeasurementImportance[]
-	): boolean | undefined {
-		const importance = measurementImportance.find(
-			(importance) => importance.measurement === humanMeasurement
-		);
-		if (!importance) return undefined;
-
-		return importance.allowTolerance;
-	}
-
-	function getTableLink(size: MatchingClothingSizeTable): string {
-		let link = `${size.name}/${size.gender}`;
-		const matchingClothingSize = size.matchingClothingSizes[0];
-		if (matchingClothingSize && isDeviationAcceptable(matchingClothingSize.deviation)) {
-			link += `?size=${matchingClothingSize.clothingSize.size}`;
-		}
-
-		return link;
-	}
 
 	let isExporting = false;
 
@@ -155,144 +91,14 @@
 <ClothingHead />
 
 <div class="p-4 flex flex-col gap-4">
-	<span class="text-xl"
-		>Hier kannst du die passende Größe für den neuen Einsatzanzug (MEA) finden. Gebe deine
-		Körpermaße und dein Geschlecht ein.</span
-	>
-
-	<div class="flex flex-row flex-wrap gap-2">
-		<Select
-			options={[
-				{ value: 'M', label: humanGenderToFriendlyString('M') },
-				{ value: 'W', label: humanGenderToFriendlyString('W') }
-			]}
-			bind:selected={$clothingInput.gender}
-			label="Geschlecht"
-		/>
-		<Input
-			bind:inputValue={$clothingInput.height}
-			type="number"
-			label={humanMeasurementToFriendlyName('height')}
-			placeholder={`${humanMeasurementToFriendlyName('height')} in cm`}
-		/>
-		<Input
-			bind:inputValue={$clothingInput.chest}
-			type="number"
-			label={humanMeasurementToFriendlyName('chestCircumference')}
-			placeholder={`${humanMeasurementToFriendlyName('chestCircumference')} in cm`}
-		/>
-		<Input
-			bind:inputValue={$clothingInput.waist}
-			type="number"
-			label={humanMeasurementToFriendlyName('waistCircumference')}
-			placeholder={`${humanMeasurementToFriendlyName('waistCircumference')} in cm`}
-		/>
-		<Input
-			bind:inputValue={$clothingInput.hip}
-			type="number"
-			label={humanMeasurementToFriendlyName('hipCircumference')}
-			placeholder={`${humanMeasurementToFriendlyName('hipCircumference')} in cm`}
-		/>
-		<Input
-			bind:inputValue={$clothingInput.insideLegLength}
-			type="number"
-			label={humanMeasurementToFriendlyName('insideLegLength')}
-			placeholder={`${humanMeasurementToFriendlyName('insideLegLength')} in cm`}
-		/>
-	</div>
+	<ClothingSizesInput />
 
 	<div class="flex flex-col gap-2">
 		{#if calculatedSizes}
 			<div class="font-bold">Es wurden folgende Größen gefunden:</div>
 			<div class="grid grid-flow-row-dense grid-cols-1 md:grid-cols-3 gap-2">
 				{#each calculatedSizes as size (size.name)}
-					<div class="flex flex-col justify-between gap-1 border-2 p-2 border-thw rounded-md">
-						<div>
-							<h2 class="text-xl font-bold">{clothingNameToFriendlyName(size.name)}:</h2>
-							{#if size.matchingClothingSizes && isDeviationAcceptable(size.matchingClothingSizes[0].deviation)}
-								<div class="flex flex-col gap-2">
-									<div>
-										<p>Konfektionsgröße: {size.matchingClothingSizes[0].clothingSize.size}</p>
-										<p
-											class={isNessaryMeasurement('height', size.measurementImportance)
-												? 'font-bold'
-												: ''}
-										>
-											{sizeToString(
-												'height',
-												size.matchingClothingSizes[0].clothingSize,
-												size.measurementImportance
-											)}
-										</p>
-										<p
-											class={isNessaryMeasurement('chestCircumference', size.measurementImportance)
-												? 'font-bold'
-												: ''}
-										>
-											{sizeToString(
-												'chestCircumference',
-												size.matchingClothingSizes[0].clothingSize,
-												size.measurementImportance
-											)}
-										</p>
-										<p
-											class={isNessaryMeasurement('waistCircumference', size.measurementImportance)
-												? 'font-bold'
-												: ''}
-										>
-											{sizeToString(
-												'waistCircumference',
-												size.matchingClothingSizes[0].clothingSize,
-												size.measurementImportance
-											)}
-										</p>
-										<p
-											class={isNessaryMeasurement('hipCircumference', size.measurementImportance)
-												? 'font-bold'
-												: ''}
-										>
-											{sizeToString(
-												'hipCircumference',
-												size.matchingClothingSizes[0].clothingSize,
-												size.measurementImportance
-											)}
-										</p>
-										<p
-											class={isNessaryMeasurement('insideLegLength', size.measurementImportance)
-												? 'font-bold'
-												: ''}
-										>
-											{sizeToString(
-												'insideLegLength',
-												size.matchingClothingSizes[0].clothingSize,
-												size.measurementImportance
-											)}
-										</p>
-									</div>
-									<div class="italic text-sm">
-										[Abweichung: {size.matchingClothingSizes[0].deviation.toFixed(2)}]
-									</div>
-								</div>
-							{:else if missingMeasurements.has(size.name)}
-								<p class="font-bold text-red-500">Fehlende Maße:</p>
-								<ul>
-									{#each missingMeasurements.get(size.name) ?? [] as missingMeasurement}
-										<li>
-											{humanMeasurementToFriendlyName(missingMeasurement)}
-											{#if isToleranceAllowedInMeasurement(missingMeasurement, size.measurementImportance) === false}
-												(genau)
-											{/if}
-										</li>
-									{/each}
-								</ul>
-							{:else}
-								<p>No matching sizes found.</p>
-							{/if}
-						</div>
-						<div>
-							<LinkButton url={getTableLink(size)} secondary>Maßtabelle ansehen</LinkButton>
-						</div>
-					</div>
+					<ClothingResultCard {size} {missingMeasurements} />
 				{/each}
 			</div>
 
