310ea2294b6cfc15dae83c56dae79af06dbc4f6a
Malte2036 <malte2036@gmail.com>
2024-08-18 22:45:53 +0200

    :sparkles: Add manuel device id input

---
+++ apps/inventar/src/lib/inventar/ManuelDeviceIdInput.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Button from '$lib/Button.svelte';
+	import Input from '$lib/Input.svelte';
+
+	export let onScan: (decodedText: string) => void;
+
+	let visible = false;
+
+	let inputValue = '';
+</script>
+
+{#if visible}
+	<div class="flex flex-row gap-4 w-full items-end">
+		<Input bind:inputValue placeholder="Inventarnummer" label="Inventarnummer des Gerätes" />
+		<Button
+			secondary
+			click={() => {
+				onScan(inputValue);
+				visible = false;
+			}}
+		>
+			Eintragen
+		</Button>
+	</div>
+{:else}
+	<Button secondary click={() => (visible = true)}>Gerät manuell hinzufügen</Button>
+{/if}
