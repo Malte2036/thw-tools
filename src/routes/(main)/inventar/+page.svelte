1b23240a0a6132963ccc65be80baca6cf80141b6
Malte2036 <malte2036@gmail.com>
2024-08-19 22:55:08 +0200

    :lipstick: Redesign inventar page

--- src/routes/(main)/inventar/+page.svelte
+++ src/routes/(main)/inventar/+page.svelte
@@ -14,6 +14,7 @@
 	} from '$lib/inventar/inventarItem';
 	import { bannerMessage } from '$lib/shared/stores/bannerMessage';
 	import type { PageData } from './$types';
+	import { dateToFriendlyString } from '$lib/utils';
 
 	export let data: PageData;
 
@@ -76,32 +77,59 @@
 	let selectedDeviceId: InventarItemDeviceId | undefined;
 </script>
 
-<div class="flex flex-col gap-2 p-4">
-	<QrScanner {onScan} />
-	<ManuelDeviceIdInput {onScan} />
+<div class="flex flex-col gap-4 p-4">
+	<div class="flex flex-col gap-2">
+		<QrScanner {onScan} />
+		<ManuelDeviceIdInput {onScan} />
+	</div>
 
-	<div>
-		<div class="font-bold">Inventarliste:</div>
-		<Table
-			header={['', 'deviceId', 'status', 'letzte aktion von', 'letze aktion am']}
-			values={data.inventarItems.map((item) => [
-				eventTypeToEmoji(item.lastEvent.type),
-				item.deviceId,
-				eventTypeToFriendlyString(item.lastEvent.type),
-				`${item.lastEvent.user.firstName ?? ''} ${item.lastEvent.user.lastName ?? ''}`,
-				new Date(item.lastEvent.date).toLocaleString('de-DE')
-			])}
-			onValueClick={(row) => {
-				console.log(row);
-				const id = row[1];
-				selectedDeviceId = id === selectedDeviceId ? undefined : id;
-			}}
-		></Table>
+	<div class="flex flex-col gap-2">
+		<div class="font-bold text-2xl">Inventarliste:</div>
+		<div class="flex flex-col gap-2">
+			{#if data.inventarItems.length === 0}
+				<p>Keine Geräte vorhanden.</p>
+			{/if}
+			{#each data.inventarItems as item}
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<div
+					class="text-xl flex flex-row items-center p-2 gap-2 bg-thw-50 border shadow-sm rounded-2xl transition-colors hover:cursor-pointer overflow-x-scroll"
+					class:selectedItem={item.deviceId === selectedDeviceId}
+					on:click={() =>
+						(selectedDeviceId = item.deviceId === selectedDeviceId ? undefined : item.deviceId)}
+				>
+					<div class="text-2xl">{eventTypeToEmoji(item.lastEvent.type)}</div>
+					<div class="flex flex-col gap-0 w-full">
+						<div class="flex flex-row gap-2 justify-between w-full">
+							<div class="text-nowrap">ID: <span class="font-bold">{item.deviceId}</span></div>
+							<div
+								class="rounded-2xl text-sm px-2 py-1 h-min bg-green-200"
+								class:isBorrowed={item.lastEvent.type === 'borrowed'}
+							>
+								{eventTypeToFriendlyString(item.lastEvent.type)}
+							</div>
+						</div>
+						<div class="flex flex-row gap-2 items-center w-full">
+							<div class="text-sm text-nowrap text-gray-500">
+								<span class="italic">
+									{item.lastEvent.user.firstName ?? ''}
+									{item.lastEvent.user.lastName ?? ''}
+								</span>
+								{' am '}
+								<span>
+									{dateToFriendlyString(new Date(item.lastEvent.date))}
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
 	</div>
 </div>
 
 {#if selectedDeviceId}
-	<InventarItemEventsList deviceId={selectedDeviceId} />
+	<InventarItemEventsList deviceId={selectedDeviceId} scrollIntoViewOnDataChange={true} />
 {/if}
 
 {#if scannedDeviceId}
@@ -112,3 +140,12 @@
 		onSubmit={submit}
 	/>
 {/if}
+
+<style lang="scss">
+	.selectedItem {
+		@apply bg-thw-300;
+	}
+	.isBorrowed {
+		@apply bg-red-200;
+	}
+</style>
