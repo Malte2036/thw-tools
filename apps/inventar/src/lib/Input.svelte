1b96cfa6bc9aa0b33a002c8e219816c02612f5b7
Malte2036 <malte2036@gmail.com>
2024-08-22 11:56:56 +0200

    :children_crossing: Add validation to input field

--- apps/inventar/src/lib/Input.svelte
+++ apps/inventar/src/lib/Input.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import type { HTMLInputAttributes } from 'svelte/elements';
+	import type { FormEventHandler, HTMLInputAttributes } from 'svelte/elements';
 
 	export let inputValue: string;
+	export let onInput: FormEventHandler<HTMLInputElement> = () => {};
 
 	export let label: string | undefined = undefined;
 	export let placeholder: string | undefined = undefined;
 
 	export let type: HTMLInputAttributes['type'] = 'text';
+	export let inputmode: HTMLInputAttributes['inputmode'] = 'text';
 
 	export let pattern: string | undefined = undefined;
 </script>
@@ -20,7 +22,9 @@
 		class="border border-black rounded-lg py-1 px-2"
 		id="inputField"
 		{...{ type }}
+		{...{ inputmode }}
 		bind:value={inputValue}
+		on:input={onInput}
 		{placeholder}
 		{pattern}
 	/>
