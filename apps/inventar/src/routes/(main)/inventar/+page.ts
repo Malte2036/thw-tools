cf206305dad575f2ff1554ba0cdf4e1d49faffcf
Malte2036 <malte2036@gmail.com>
2024-08-30 08:37:44 +0200

    :recycle: Add "no org" page

--- apps/inventar/src/routes/(main)/inventar/+page.ts
+++ apps/inventar/src/routes/(main)/inventar/+page.ts
@@ -11,30 +11,32 @@ export const load = (async () => {
 		console.log('Not authenticated');
 
 		await login();
-		return {
-			inventarItems: [],
-			inventarItemEventBulks: []
-		};
+		return {};
 	}
 
-	const [organisation, inventarItems, inventarItemEventBulks] = await Promise.all([
-		getOrganisationForUser().then((org) => {
-			org.members.sort((a, b) => userToFriendlyString(a).localeCompare(userToFriendlyString(b)));
-			return org;
-		}),
-		getInventarItems().then((items) =>
-			items.sort(
-				(a, b) => new Date(b.lastEvent.date).getTime() - new Date(a.lastEvent.date).getTime()
+	try {
+		const [organisation, inventarItems, inventarItemEventBulks] = await Promise.all([
+			getOrganisationForUser().then((org) => {
+				org.members.sort((a, b) => userToFriendlyString(a).localeCompare(userToFriendlyString(b)));
+				return org;
+			}),
+			getInventarItems().then((items) =>
+				items.sort(
+					(a, b) => new Date(b.lastEvent.date).getTime() - new Date(a.lastEvent.date).getTime()
+				)
+			),
+			getInventarItemEventBulks().then((bulks) =>
+				bulks.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
 			)
-		),
-		getInventarItemEventBulks().then((bulks) =>
-			bulks.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
-		)
-	]);
+		]);
 
-	return {
-		inventarItems,
-		inventarItemEventBulks,
-		organisation
-	};
+		return {
+			inventarItems,
+			inventarItemEventBulks,
+			organisation
+		};
+	} catch (error) {
+		console.error(error);
+		return {};
+	}
 }) satisfies PageLoad;
