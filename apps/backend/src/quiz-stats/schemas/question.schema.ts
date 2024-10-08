20c1e80e59fe96315dd4c8ef059e5b17f5fb73ea
Malte2036 <malte2036@gmail.com>
2024-10-08 11:47:29 +0200

    :sparkles: Add radio quiz

--- apps/backend/src/quiz-stats/schemas/question.schema.ts
+++ apps/backend/src/quiz-stats/schemas/question.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
-export type QuizType = 'ga' | 'agt' | 'cbrn';
+export type QuizType = 'ga' | 'agt' | 'cbrn' | 'radio';
 
 export type QuestionDocument = HydratedDocument<Question>;
 
