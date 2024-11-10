1dacb5d3889ab4da8cbfc1fd1ea113f847694af5
Malte2036 <malte2036@gmail.com>
2024-11-10 19:08:15 +0100

    :arrow_up: Bump @malte2036/thw-tools-components to version 0.2.34 and update Table component to handle row clicks

--- apps/thw-tools/src/lib/Table.svelte
+++ apps/thw-tools/src/lib/Table.svelte
@@ -3,6 +3,15 @@
 	export let values: string[][];
 	export let onValueClick: ((row: string[], index: number) => void) | undefined = undefined;
 	export let selectedIndex: number | undefined = undefined;
+
+	function handleRowClick(event: CustomEvent<{ row: string[]; index: number }>) {
+		const { row, index } = event.detail;
+		selectedIndex = index;
+		if (onValueClick) {
+			onValueClick(row, index);
+		}
+	}
 </script>
 
-<thw-table {header} {values} {onValueClick} {selectedIndex} maxHeight={600}></thw-table>
+<thw-table {header} {values} {selectedIndex} maxHeight={600} onrow-click={handleRowClick}
+></thw-table>
