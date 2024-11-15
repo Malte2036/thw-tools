db4e1b0ffc179e81d03cc1aa83602f4a944948f3
Malte2036 <malte2036@gmail.com>
2024-11-15 13:29:01 +0100

    :heavy_plus_sign: Add capacitor and remove sitemap.xml

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'de.maltesehmer.thwtools',
+	appName: 'THW-Tools',
+	webDir: 'build',
+	bundledWebRuntime: false
+};
+
+export default config;
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
 		paths: {
 			relative: false
 		}
