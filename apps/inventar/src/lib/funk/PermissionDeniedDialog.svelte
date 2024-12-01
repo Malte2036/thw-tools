8139c3a075572401861732b57f703eb162933a8c
Malte2036 <malte2036@gmail.com>
2024-12-01 12:48:00 +0100

    :recycle: Add dialog if camera permission is denied

---
+++ apps/inventar/src/lib/funk/PermissionDeniedDialog.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Button from '$lib/Button.svelte';
+	import Dialog from '$lib/Dialog.svelte';
+
+	export let onClose: () => void;
+</script>
+
+<Dialog title="Kamera-Zugriff nicht erlaubt">
+	<div slot="content" class="flex flex-col gap-2">
+		<div>
+			<p class="font-bold">Der Zugriff auf die Kamera wurde nicht erlaubt.</p>
+			<p>Bitte erlaube den Zugriff auf die Kamera im Browser, um QR-Codes scannen zu können.</p>
+		</div>
+		<ul>
+			<li>
+				<strong>Chrome:</strong> Klicke auf das Einstellungs-Symbol in der Adressleiste vor der URL
+				und setze die Kamera auf <em>"Zulassen"</em>.
+			</li>
+			<li>
+				<strong>Firefox:</strong> Klicke auf das Schloss-Symbol in der Adressleiste vor der URL und
+				setze die Kamera auf <em>"Zulassen"</em>.
+			</li>
+			<li>
+				<strong>Safari (iOS 18+):</strong> Klicke auf das Symbol links neben der URL. Klicke auf die
+				drei Punkte und unter <em>Website Settings</em> setze die Kamera auf <em>"Zulassen"</em>.
+			</li>
+			<li>
+				<strong>Safari (iOS 17-):</strong> Tippe auf das <em>aA-Symbol</em> links in der
+				Adressleiste. Gehe zu <em>Website-Einstellungen</em> und setzte <em>Kamera</em> auf
+				<em>"Zulassen"</em>.
+			</li>
+		</ul>
+		<p>
+			Nachdem du die Einstellungen geändert hast, lade diese Seite neu, um es erneut zu versuchen.
+		</p>
+	</div>
+
+	<div slot="footer">
+		<Button click={onClose}>Schließen</Button>
+	</div>
+</Dialog>
--- apps/inventar/src/lib/funk/QRScanner.svelte
+++ apps/inventar/src/lib/funk/QRScanner.svelte
@@ -2,6 +2,7 @@
 	import Button from '$lib/Button.svelte';
 	import { Html5Qrcode, type Html5QrcodeCameraScanConfig } from 'html5-qrcode';
 	import { onDestroy, onMount } from 'svelte';
+	import PermissionDeniedDialog from './PermissionDeniedDialog.svelte';
 
 	interface Props {
 		onScan: (decodedText: string) => void;
@@ -16,6 +17,7 @@
 	}: Props = $props();
 
 	let scanning = $state(false);
+	let permissionDenied = $state(false);
 
 	let html5Qrcode: Html5Qrcode;
 
@@ -37,25 +39,40 @@
 		experimentalFeatures?: { useBarCodeDetectorIfSupported: boolean };
 	}
 
-	function start() {
-		const config: ExtendedHtml5QrcodeCameraScanConfig = {
-			fps: 10,
-			qrbox: qrboxFunction,
-			aspectRatio: 1,
-			focusMode: 'continuous',
-			advanced: [{ zoom: 2.0 }],
-			experimentalFeatures: {
-				useBarCodeDetectorIfSupported: true
-			}
-		};
+	async function start() {
+		try {
+			const config: ExtendedHtml5QrcodeCameraScanConfig = {
+				fps: 10,
+				qrbox: qrboxFunction,
+				aspectRatio: 1,
+				focusMode: 'continuous',
+				advanced: [{ zoom: 2.0 }],
+				experimentalFeatures: {
+					useBarCodeDetectorIfSupported: true
+				}
+			};
 
-		html5Qrcode.start({ facingMode: 'environment' }, config, onScanSuccess, onScanFailure);
-		scanning = true;
+			scanning = true;
+			await html5Qrcode.start({ facingMode: 'environment' }, config, onScanSuccess, onScanFailure);
+		} catch (error) {
+			scanning = false;
+
+			if (typeof error === 'string' && error.includes('NotAllowedError')) {
+				permissionDenied = true;
+			} else {
+				console.error(error);
+			}
+		}
 	}
 
 	async function stop() {
-		await html5Qrcode.stop();
-		scanning = false;
+		try {
+			await html5Qrcode.stop();
+		} catch (error) {
+			console.error(error);
+		} finally {
+			scanning = false;
+		}
 	}
 
 	function onScanSuccess(decodedText: string, decodedResult: any) {
@@ -69,7 +86,8 @@
 	let qrboxFunction = function (viewfinderWidth: number, viewfinderHeight: number) {
 		let minEdgePercentage = 0.7; // 70%
 		let minEdgeSize = Math.min(viewfinderWidth, viewfinderHeight);
-		let qrboxSize = Math.floor(minEdgeSize * minEdgePercentage);
+		let qrboxSize = Math.max(100, Math.floor(minEdgeSize * minEdgePercentage));
+
 		return {
 			width: qrboxSize,
 			height: qrboxSize
@@ -86,5 +104,6 @@
 	{/if}
 </div>
 
-<style>
-</style>
+{#if permissionDenied}
+	<PermissionDeniedDialog onClose={() => (permissionDenied = false)} />
+{/if}
