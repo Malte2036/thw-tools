686458a8ef657ef493f3b690b1765d2ce2a8ebba
Malte2036 <malte2036@gmail.com>
2024-12-18 17:57:28 +0100

    :recycle: Enhance settings store to persist selected camera in local storage

--- apps/inventar/src/lib/shared/stores/settingsStore.ts
+++ apps/inventar/src/lib/shared/stores/settingsStore.ts
@@ -1,9 +1,36 @@
-import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+import { writable, type Writable } from 'svelte/store';
 
 export type SettingsData = {
 	selectedCamera: string | null;
 };
 
-export const settings = writable<SettingsData>({
+const defaultSettings: SettingsData = {
 	selectedCamera: null
+};
+
+const localStorageKey = 'settings';
+function getSavedSettings(): SettingsData {
+	if (!browser) return defaultSettings;
+
+	const stored = localStorage.getItem(localStorageKey);
+	if (stored) {
+		try {
+			return JSON.parse(stored);
+		} catch (e) {
+			console.error('Error parsing settings from local storage', e);
+			return defaultSettings;
+		}
+	}
+	return defaultSettings;
+}
+
+export const settings: Writable<SettingsData> = writable<SettingsData>(getSavedSettings(), () => {
+	if (!browser) return;
+
+	const unsubscribe = settings.subscribe((value: SettingsData) =>
+		localStorage.setItem(localStorageKey, JSON.stringify(value))
+	);
+
+	return unsubscribe;
 });
