3722b553f361a6f0aaee5f2385df946c3dfd56f5
Malte2036 <malte2036@gmail.com>
2025-01-14 18:05:04 +0100

    :sparkles: Add ProgressBar component and integrate it into quiz page for visual progress tracking

---
+++ apps/thw-tools/src/lib/quiz/ProgressBar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let progress: number;
+</script>
+
+<div class="w-full h-2 bg-gray">
+	<div class="h-full bg-thw rounded-r-full" style="width: {progress * 100}%"></div>
+</div>
