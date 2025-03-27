c2f4b531da33439e66a5f8ea79ef85aa765242bb
Malte Sehmer <malte2036@gmail.com>
2025-03-27 23:15:30 +0100

    :sparkles: Add VehicleIcon component and integrate vehicle management link in navigation

--- apps/inventar/src/routes/(main)/(inventar)/fahrzeuge/+page.svelte
+++ apps/inventar/src/routes/(main)/(inventar)/fahrzeuge/+page.svelte
@@ -148,27 +148,7 @@
 	}
 </script>
 
-<div class="px-4 py-6">
-	<div class="mb-6">
-		<LinkButton url="../" secondary>
-			<span class="flex items-center">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-4 w-4 mr-2"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-				Zurück zur Inventar-Übersicht
-			</span>
-		</LinkButton>
-	</div>
-
+<div class="px-4">
 	<div class="bg-white rounded-lg shadow-md p-6">
 		<div class="flex flex-col md:flex-row justify-between items-start md:items-center gap-4 mb-6">
 			<div>
