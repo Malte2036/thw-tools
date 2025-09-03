d01ef5bb79321ddf81a237613f59ad809068135f
Malte Sehmer <malte2036@gmail.com>
2025-09-03 03:01:59 +0200

    :sparkles: add kinde auth

---
+++ apps/inventar-next/src/provider/auth/authProvider.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { KindeProvider } from '@kinde-oss/kinde-auth-react';
+
+const getKindeSettings = () => {
+  const KINDE_DOMAIN = process.env.NEXT_PUBLIC_KINDE_DOMAIN;
+  const KINDE_CLIENT_ID = process.env.NEXT_PUBLIC_KINDE_API_CLIENT;
+
+  console.log(KINDE_DOMAIN, KINDE_CLIENT_ID);
+
+  if (!KINDE_DOMAIN || !KINDE_CLIENT_ID) {
+    throw new Error(`NEXT_PUBLIC_KINDE_DOMAIN or NEXT_PUBLIC_KINDE_CLIENT_ID is not set`);
+  }
+
+  return {
+    domain: KINDE_DOMAIN,
+    clientId: KINDE_CLIENT_ID,
+  };
+};
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const kindeSettings = getKindeSettings();
+
+  const origin = typeof window !== 'undefined' ? window.location.origin : '';
+
+  return (
+    <KindeProvider
+      clientId={kindeSettings.clientId}
+      domain={kindeSettings.domain}
+      logoutUri={origin}
+      redirectUri={origin}
+      callbacks={{
+        onSuccess: (user) => {
+          console.log(`Successfully authenticated user: ${user.email}`, user);
+        },
+        onError: (error) => {
+          console.log('onError', error);
+        },
+        onEvent: (event) => {
+          console.log('onEvent', event);
+        },
+      }}
+    >
+      {children}
+    </KindeProvider>
+  );
+};
