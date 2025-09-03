38dd0ee6fbfc92eee1f865397f21f69dfce7842b
Malte Sehmer <malte2036@gmail.com>
2025-09-03 02:30:12 +0200

    :tada: setup inventar nextjs project with proxy

---
+++ apps/inventar-next/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    const svelteKitUrl = process.env.SVELTEKIT_URL;
+    console.log(`Forwarding Next.js requests to SvelteKit URL: ${svelteKitUrl}`);
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: `${svelteKitUrl}/:path*`,
+        },
+      ],
+    };
+  },
+};
+
+export default nextConfig;
